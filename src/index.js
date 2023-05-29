cdad61aab305e0b73257dbc78f2a6ac78f726723
Thrasos06 <thrasos.vito@gmail.com>
2023-05-29 15:10:28 +0300

    Project upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { searchRobots, requestRobots } from "./reducers";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import "./index.css";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
+import "tachyons";
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
